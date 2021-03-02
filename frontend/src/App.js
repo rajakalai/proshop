8c8f93914591983c15b593058f5f8d3f232999cd
rajakalai <71640279+rajakalai@users.noreply.github.com>
2021-03-03 00:46:44 +0530

    navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
-
+import Header from './components/Header';
+import Footer from './components/Footer';
+import {Container} from 'react-bootstrap'
 function App() {
   return (
     <>
+    <Header></Header>
+    <main className="py-3">
+      <Container>
       <h1>Welcome to proshop</h1>
+      </Container>      
+    </main>      
+      <Footer></Footer>
     </>
   );
 }
